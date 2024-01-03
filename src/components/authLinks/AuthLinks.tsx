895928d549108f48b5441aa74d51695bf06ee54d
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-03 15:11:49 +0700

    feat: create side menu

--- src/components/authLinks/AuthLinks.tsx
+++ src/components/authLinks/AuthLinks.tsx
@@ -21,6 +21,9 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
+          <span className={styles.link} onClick={() => signOut()}>
+            Logout
+          </span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
@@ -33,6 +36,14 @@ const AuthLinks = () => {
           <Link href="/">Homepage</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
         </div>
       )}
     </>
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,10 +1,53 @@
 import React from "react";
 import styles from "./cardList.module.css";
+import Card from "../card/Card";
 
 const CardList = async () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card
+          item={{
+            img: "/p1.jpeg",
+            createdAt: "03.01.2024",
+            catSlug: "CULTURE",
+            desc: "lorem",
+          }}
+        />
+        <Card
+          item={{
+            img: "/p1.jpeg",
+            createdAt: "03.01.2024",
+            catSlug: "CULTURE",
+            desc: "lorem",
+          }}
+        />
+        <Card
+          item={{
+            img: "/p1.jpeg",
+            createdAt: "03.01.2024",
+            catSlug: "CULTURE",
+            desc: "lorem",
+          }}
+        />
+        <Card
+          item={{
+            img: "/p1.jpeg",
+            createdAt: "03.01.2024",
+            catSlug: "CULTURE",
+            desc: "lorem",
+          }}
+        />
+        <Card
+          item={{
+            img: "/p1.jpeg",
+            createdAt: "03.01.2024",
+            catSlug: "CULTURE",
+            desc: "lorem",
+          }}
+        />
+      </div>
     </div>
   );
 };
