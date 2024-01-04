481e47fc0f0cf4c46c0d8fe0c0c3eec26dfbd349
Nghi Nguyen Thanh <nghi.nguyen@evehr.vn>
2024-01-04 16:57:51 +0700

    Versioning untracked files

--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./cardList.module.css";
+
 import Card from "../card/Card";
 import Pagination from "../pagination/Pagination";
 import axios from "axios";
@@ -33,9 +33,9 @@ const CardList = async ({ page, cat }: Props) => {
   const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Recent Posts</h1>
-      <div className={styles.posts}>
+    <div>
+      <h1>Recent Posts</h1>
+      <div>
         {posts?.map((item: any) => (
           <Card item={item} key={item._id} />
         ))}
